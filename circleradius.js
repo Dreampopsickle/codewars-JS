72a424b61f395f572af7dce1325a194be08df3de
Dreampopsickle <132526661+Dreampopsickle@users.noreply.github.com>
2023-09-12 19:02:56 -0400

    replace round to parsefloat

--- circleradius.js
+++ circleradius.js
@@ -5,7 +5,7 @@
 function circleArea(radius) {
     //area of circle is pi * r ** 2
     const area = (Math.PI) * (radius ** 2);
-    let roundedArea = Math.round((area * 1000) / 1000);
+    let roundedArea = parseFloat(area.toFixed(3));
     if (radius <= 0) {
         throw new Error('Radius must be positive');
     } else {
